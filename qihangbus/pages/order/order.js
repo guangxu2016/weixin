5fb2e24696c3582ab41257d17709b435ee344e0e
run <guangxu2016@foxmail.com>
2018-01-10 23:38:26 +0800

    微信小程序

---
+++ qihangbus/pages/order/order.js
@@ -0,0 +1,99 @@
+// pages/order/order.js
+var Zan = require('../../template/contract.js');
+Page(Object.assign({}, Zan.Tab,{
+
+  /**
+   * 页面的初始数据
+   */
+   data: {
+    tab1: {
+      list: [{
+        id: 0,
+        title: '全部'
+      }, {
+        id: 1,
+        title: '待付款'
+      }, {
+        id: 2,
+        title: '待发货'
+      }, {
+        id: 3,
+        title: '待收货'
+      }, {
+        id: 4,
+        title: '待评价'
+      }],
+      selectedId: 0,
+      scroll: false,
+    },
+  },
+  handleZanTabChange(e) {
+    var componentId = e.componentId;
+    var selectedId = e.selectedId;
+    this.setData({
+      `${componentId}.selectedId`: selectedId
+      });
+
+}
+})),
+  
+
+
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
